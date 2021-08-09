3ce1bd4fc1f15a3b8ad1f631b4ab4085e599e0e1
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-09 16:02:05 -0500

    Update

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+
+		<!-- <form method"GET">
+			<input type="text" name="person">
+			<button>SUBMIT</button>
+		</form> -->
+
+        <?php
+
+            $name = $_GET['person'];
+
+            echo $name." is a great baby!";
+
+			echo "      ";
+
+			echo strlen("Hi Shy!");
+
+
+        ?>
+   
+    </body>
+</html>
